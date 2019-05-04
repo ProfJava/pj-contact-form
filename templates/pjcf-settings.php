753665f9abf3bf68d56f760b6e32f13b079c5f25
ProfJava <prof.saaad@gmail.com>
2019-05-04 12:22:00 +0200

    Fixed - Settings Page

--- templates/pjcf-settings.php
+++ templates/pjcf-settings.php
@@ -1,25 +1,35 @@
 <div class="wrap">
     <h1>PJ Contact Form Settings</h1>
-    <table class="form-table pjcf-email-info">
-        <tbody>
-        <tr>
-            <th scope="row"><?php esc_html_e('Use The Shortcode To display The Contact Form : ','pjcf'); ?></th>
-            <td>
-                <p>[pjcf]</p>
-            </td>
-        </tr>
-        <tr>
-            <th scope="row"><?php esc_html_e('Receiver Email : ','pjcf'); ?></th>
-            <td>
-                <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
+    <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
+        <table class="form-table pjcf-email-info">
+            <tbody>
+            <tr>
+                <th scope="row"><?php esc_html_e('Use The Shortcode To display The Contact Form : ','pjcf'); ?></th>
+                <td>
+                    <p>[pjcf]</p>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row"><?php esc_html_e('Receiver Email : ','pjcf'); ?></th>
+                <td>
                     <?php wp_nonce_field('receiver_email','wp_nonce_check') ?>
                     <input type="hidden" name="action" value="receiver_email_process">
                     <input type="email" name="receiver_email" value="<?php echo esc_attr(get_option('receiver_email')) ?>" class="regular-text">
-                    <input type="email" name="receiver_email" value="<?php echo esc_attr(get_option('sender_email')) ?>" class="regular-text">
-                    <button type="submit" class="button button-primary">Save</button>
-                </form>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row"><?php esc_html_e('Sender Email : ','pjcf'); ?></th>
+                <td>
+                    <input type="email" name="sender_email" value="<?php echo esc_attr(get_option('sender_email')) ?>" class="regular-text">
+                </td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>
+                    <button type="submit" class="button button-primary"><?php _e('Save','pjcf') ?></button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
 </div>
